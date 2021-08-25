b918c9d8881b72a1ae64ce2a1f2d2163577cac55
Marcin Bunsch <marcin.bunsch@gmail.com>
2021-08-25 13:14:10 +0200

    Add dsl

--- lib/puma/kubernetes/dsl.rb
+++ lib/puma/kubernetes/dsl.rb
@@ -3,5 +3,8 @@ module Puma
     def kubernetes_url(url)
       @options[:kubernetes_url] = url
     end
+    def kubernetes_stats_hook(&block)
+      @options[:kubernetes_stats_hook] = block
+    end
   end
 end
